50ec6460f531aa61851ab53fb58f904a3422db5d
postwo <codo7717@naver.com>
2025-08-29 01:46:16 +0900

    세팅

---
+++ src/main/java/com/example/altteulmoa_project/AltteulmoaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.altteulmoa_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltteulmoaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltteulmoaProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/altteulmoa_project/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.altteulmoa_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
